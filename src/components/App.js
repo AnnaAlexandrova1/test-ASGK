4c5e5dc126de69107c5593f912766cb96cd2aca8
Anna <anna4776497@gmail.com>
2023-02-08 16:27:01 +0300

    стилизация

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,13 @@ import './App.css';
 
 
 function App() {
-  const auth = useSelector(state => state.auth.isAuth)
-  console.log(auth)
+  const isAuth = useSelector(state => state.auth.isAuth)
 
   return (
     <div className="app">
       <Navbar />
-      {/* <div className="wrap"> */}
-         <Authorization />
-      {/* </div> */}
+      {!isAuth && <Authorization />}
+
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,31 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { logout } from '../../reducers/authReduser'
-import './Navbar.css'
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../reducers/authReduser";
+import "./Navbar.css";
 
 export default function Navbar() {
-    const isAuth = useSelector(state => state.auth.isAuth)
-    const currentUser = useSelector(state => state.auth.currentUser)
-    const dispatch = useDispatch()
-    
-    return (
-         <div className="wrap-header">
-            <div className="navbar navbar-expand-lg bg-body-tertiary">
-                    <div className="outh-out-block">
-                <h1 className="text-center">RFM-анализ</h1>
-                    </div>
-                
-                {isAuth &&
-                    <div className="out-block-user">
-                        {isAuth && <span className="navbar-text">{currentUser}</span>}
-                        {isAuth && <button type="button" className="btn btn-secondary" onClick={() => dispatch(logout())}>Выход</button>}
-                    </div>}
-                
+  const isAuth = useSelector((state) => state.auth.isAuth);
+  const currentUser = useSelector((state) => state.auth.currentUser);
+  const dispatch = useDispatch();
 
-
-            </div>
-            </div>
-    )
+  return (
+    <div className="wrap-header">
+      <div className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="outh-out-block">
+          <h1 className="text-center">RFM-анализ</h1>
+        </div>
+      </div>
+      {isAuth && (
+        <div className="out-block-user">
+          <span className="navbar-text">{currentUser}</span>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => dispatch(logout())}
+          >
+            Выход
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
